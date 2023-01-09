ea77c42ca298b38f0276b7bc96cb8582ef21c8cf
Adeyanju Adeyemi <alex.olaolu007@gmail.com>
2023-01-09 21:42:23 +0100

    feat: Added support for glsl files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      "exclude": /node_modules/,
+      "use": ["raw-loader", "glslify-loader"]
+    });
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
